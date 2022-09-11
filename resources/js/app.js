d504cb2b0e28b29e84202e84da4e6502f9152e7f
Jorge Betancur <jabetancur12@gmail.com>
2022-09-11 10:26:42 -0500

    refactor

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,28 +4,28 @@ import { createApp, defineComponent } from "vue";
 
 import router from './router'
 import App from './App.vue'
-import ProductsIndex from './components/products/ProductsIndex'
-import ProductsCreate from './components/products/ProductsCreate'
-import ProductsEdit from './components/products/ProductsEdit'
+// import ProductsIndex from './components/products/ProductsIndex'
+// import ProductsCreate from './components/products/ProductsCreate'
+// import ProductsEdit from './components/products/ProductsEdit'
 
-import SuppliersIndex from './components/suppliers/SuppliersIndex'
-import SuppliersCreate from './components/suppliers/SuppliersCreate'
-import SuppliersEdit from './components/suppliers/SuppliersEdit'
+// import SuppliersIndex from './components/suppliers/SuppliersIndex'
+// import SuppliersCreate from './components/suppliers/SuppliersCreate'
+// import SuppliersEdit from './components/suppliers/SuppliersEdit'
 
-import PurchasesIndex from './components/purchases/PurchasesIndex'
-import PurchasesCreate from './components/purchases/PurchasesCreate'
-import PurchasesEdit from './components/purchases/PurchasesEdit'
-import PurchasesGeneral from './components/purchases/PurchasesGeneral'
+// import PurchasesIndex from './components/purchases/PurchasesIndex'
+// import PurchasesCreate from './components/purchases/PurchasesCreate'
+// import PurchasesEdit from './components/purchases/PurchasesEdit'
+// import PurchasesGeneral from './components/purchases/PurchasesGeneral'
 
-import PurchaseDetailsIndex from './components/purchaseDetails/PurchaseDetailsIndex.vue'
-import PurchaseDetailsCreate from './components/purchaseDetails/PurchaseDetailsCreate.vue'
-import PurchaseDetailsEdit from './components/purchaseDetails/PurchaseDetailsEdit.vue'
+// import PurchaseDetailsIndex from './components/purchaseDetails/PurchaseDetailsIndex.vue'
+// import PurchaseDetailsCreate from './components/purchaseDetails/PurchaseDetailsCreate.vue'
+// import PurchaseDetailsEdit from './components/purchaseDetails/PurchaseDetailsEdit.vue'
 
-import Pagination from "laravel-vue-pagination";
+// import Pagination from "laravel-vue-pagination";
 
 
 const root = defineComponent({
-  App
+    App
 })
 
 const app = createApp(root)
@@ -33,22 +33,22 @@ const app = createApp(root)
 app.use(router)
 
 app.component('App', App)
-app.component('ProductsIndex', ProductsIndex);
-app.component('ProductsCreate', ProductsCreate);
-app.component('ProductsEdit', ProductsEdit);
+// app.component('ProductsIndex', ProductsIndex);
+// app.component('ProductsCreate', ProductsCreate);
+// app.component('ProductsEdit', ProductsEdit);
 
-app.component('SuppliersIndex', SuppliersIndex);
-app.component('SuppliersCreate', SuppliersCreate);
-app.component('SuppliersEdit', SuppliersEdit);
+// app.component('SuppliersIndex', SuppliersIndex);
+// app.component('SuppliersCreate', SuppliersCreate);
+// app.component('SuppliersEdit', SuppliersEdit);
 
-app.component('PurchasesIndex', PurchasesIndex);
-app.component('PurchasesCreate', PurchasesCreate);
-app.component('PurchasesEdit', PurchasesEdit);
+// app.component('PurchasesIndex', PurchasesIndex);
+// app.component('PurchasesCreate', PurchasesCreate);
+// app.component('PurchasesEdit', PurchasesEdit);
 
-app.component('PurchaseDetailsIndex', PurchaseDetailsIndex);
-app.component('PurchaseDetailsCreate', PurchaseDetailsCreate);
-app.component('PurchaseDetailsEdit', PurchaseDetailsEdit);
+// app.component('PurchaseDetailsIndex', PurchaseDetailsIndex);
+// app.component('PurchaseDetailsCreate', PurchaseDetailsCreate);
+// app.component('PurchaseDetailsEdit', PurchaseDetailsEdit);
 
-app.component('Pagination', Pagination)
+// app.component('Pagination', Pagination)
 
 app.mount("#app")
